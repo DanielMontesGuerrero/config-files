b2551429af04df37daff7e4e0470c810e04589d3
Daniel Montes Guerrero <43693529+DanielMontesGuerrero@users.noreply.github.com>
2021-10-17 15:34:45 -0500

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,8 @@ neofetch
 set fish_greeting
 
 alias vim "nvim"
-alias openICPC "cd /media/win//Users/danim/Documents/ICPC/"
-alias openWorkspace "cd /media/win/Users/danim/Documents/Proyectos/"
-alias openSchool "cd /media/win/Users/danim/Documents/ESCOM/Semestre\ 6/"
-alias mountWinDisk "sudo mount -t ntfs-3g /dev/sdb1 /media/win/"
+alias openICPC "cd ~/Documents/ICPC/"
+alias openWorkspace "cd ~/Documents/Workspace/"
+alias openSchool "cd ~/Documents/ESCOM/Semestre\ 7/"
 alias configTablet "xsetwacom set \"GAOMON Gaomon Tablet Pad pad\" button 1 key Ctrl Z && xsetwacom set \"GAOMON Gaomon Tablet Pad pad\" button 2 key Alt tab"
-alias focusTabletHDMI "xsetwacom set \"GAOMON Gaomon Tablet stylus\" MapToOutput HDMI-1"
+alias focusTabletHDMI "xsetwacom set \"GAOMON Gaomon Tablet stylus\" MapToOutput HDMI-A-0"
