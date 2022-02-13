aadf0d57a367bb503921ee19dc80bf8069e886cb
Daniel Montes Guerrero <43693529+DanielMontesGuerrero@users.noreply.github.com>
2022-02-13 14:28:26 -0600

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,23 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 neofetch
 set fish_greeting
 
 alias vim "nvim"
 alias openICPC "cd ~/Documents/ICPC/"
 alias openWorkspace "cd ~/Documents/Workspace/"
-alias openSchool "cd ~/Documents/ESCOM/Semestre\ 7/"
+alias openSchool "cd ~/Documents/ESCOM/Semestre\ 8/"
 alias configTablet "xsetwacom set \"GAOMON Gaomon Tablet Pad pad\" button 1 key Ctrl Z && xsetwacom set \"GAOMON Gaomon Tablet Pad pad\" button 2 key Alt tab"
-alias focusTabletHDMI "xsetwacom set \"GAOMON Gaomon Tablet stylus\" MapToOutput HDMI-A-0"
+alias focusTabletHDMI "xsetwacom set \"GAOMON Gaomon Tablet Pen stylus\" MapToOutput HDMI-A-0"
+
+set --export PATH "$HOME/.local/bin:$PATH"
+set --export ANDROID_HOME "$HOME/Android/Sdk"
+set --export PATH "$ANDROID_HOME/emulator:$PATH"
+set --export PATH "$ANDROID_HOME/tools:$PATH"
+set --export PATH "$ANDROID_HOME/tools/bin:$PATH"
+set --export PATH "$ANDROID_HOME/platform-tools:$PATH"
+set --export PATH "/home/linuxbrew/.linuxbrew/bin:$PATH"
+set --export MANPATH "/home/linuxbrew/.linuxbrew/share/man:$MANPATH"
+set --export INFOPATH "/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"
